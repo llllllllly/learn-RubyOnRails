9b6551cbed3c0a5918e2253f3556fcff0e041ef8
木水 <lc564128352@gmail.com>
2018-09-14 20:06:16 +0800

    8.RoR 教程第一章练习完成

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hola
+    render html: "¡Hola, mundo!"
+  end
+
+  def goodbye
+    render html: "Good bye, World!"
+  end
 end
